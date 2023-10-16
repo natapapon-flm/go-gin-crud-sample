8a553a5efbdcaa6c1ce4db69a742a699081e0bda
natapapon <natapapon.r@gmail.com>
2023-10-16 17:59:05 +0700

    remove json binding at item model

--- models/item.go
+++ models/item.go
@@ -2,9 +2,9 @@ package models
 
 type Item struct {
 	Id        string `gorm:"primaryKey;not null;size:255"`
-	ItemName  string `json:"item_name" gorm:"size:255"`
-	ItemPrice  float64 `json:"item_price"`
-	Amount     int `json:"amount"`
+	ItemName  string `gorm:"size:255"`
+	ItemPrice  float64 
+	Amount     int
 }
 
 type ItemRequst struct {
